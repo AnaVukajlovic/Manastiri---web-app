3789018edd9d1a3a05fb07fa827b72acf59be32a
Ana Vukajlović <147943106+AnaVukajlovic@users.noreply.github.com>
2025-12-10 17:38:32 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Dodaj sesiju
+builder.Services.AddSession();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+// Ako imaš UseStaticFiles u šablonu, zadrži ga; ako koristiš MapStaticAssets kao ovde, to je već rešeno routingom za statiku.
+app.UseRouting();
+
+// Uključi sesiju PRE Authorization/Endpoints
+app.UseSession();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+app.Run();
